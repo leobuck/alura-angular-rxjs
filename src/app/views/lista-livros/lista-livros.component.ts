7176ab7c11fb1a1ce709fe32930ca41ed755d2a1
leobuck <leobuck12@gmail.com>
2023-10-12 19:11:51 -0300

    Desafio: qual operador posso usar?

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { debounceTime, filter, map, switchMap, tap } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs';
 import { Item } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
@@ -22,6 +22,7 @@ export class ListaLivrosComponent {
     debounceTime(PAUSA),
     filter((valorDigitado) => valorDigitado.length >= 3),
     tap(() => console.log('Fluxo inical')),
+    distinctUntilChanged(),
     switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
     tap((retornoApi) => console.log(retornoApi)),
     map((items) => this.livrosResultadoParaLivros(items))
